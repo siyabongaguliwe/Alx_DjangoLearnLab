34a6e1a4629f2c2d1c6279afbbffb38f19ab3376
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2025-09-23 14:51:47 +0200

    updates

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -7,3 +7,17 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')                     # Search bar fields
 
 admin.site.register(Book, BookAdmin)
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
