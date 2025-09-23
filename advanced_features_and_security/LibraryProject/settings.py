3bd16e49dd6e2ef7b9b6c34ab7852883a4c3dc11
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2025-09-23 23:57:51 +0200

    updates

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -142,5 +142,4 @@ CSP_DEFAULT_SRC = ("'self'",)
 CSP_SCRIPT_SRC = ("'self'", "https://trusted.cdn.com")
 CSP_STYLE_SRC = ("'self'", "https://trusted.cdn.com")
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
+["SECURE_PROXY_SSL_HEADER", "HTTP_X_FORWARDED_PROTO"]
