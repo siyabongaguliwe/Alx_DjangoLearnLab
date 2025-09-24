dcd35b8b626ba6d12b10df2aed4019be4f1bdf1f
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2025-09-24 19:45:29 +0200

    updates

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -8,3 +8,11 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Connects to api/urls.py
+]
