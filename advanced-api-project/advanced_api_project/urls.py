ada38d1a9c2608902738080d031c8ca7e36a4811
siyabongaguliwe <siyabongaguliwe1@gmail.com>
2025-09-28 13:05:27 +0200

    updates

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -29,5 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/', include('api.urls')),
-
+    path('api/', include('api.urls')),
 ]
+
+
